5243d119ca56e5829b783cf92daedcbc08f76b72
Stinky-c <60587749+Stinky-c@users.noreply.github.com>
2022-05-05 22:32:20 -0700

    reddit downloader prototype Its getting harder

--- test.py
+++ test.py
@@ -13,31 +13,54 @@ if re.match(urlreg,link2):
 '''
 
 
-video = "C:\Projects\DisBot\page2.html" # https://www.reddit.com/r/ProgrammerHumor/comments/uiw8wh/
-gif = "C:\Projects\DisBot\page.html"    # https://www.reddit.com/r/standardissuecat/comments/ui22c7/
-image = "C:\Projects\DisBot\page3.html" # https://www.reddit.com/r/softwaregore/comments/uj0mdr/
-error = "C:\Projects\DisBot\page4.html" # https://redditsave.com/info?url=
+video = ".\page2.html"  # https://www.reddit.com/r/ProgrammerHumor/comments/uiw8wh/
+gif = ".\page.html"     # https://www.reddit.com/r/standardissuecat/comments/ui22c7/
+image = ".\page3.html"  # https://www.reddit.com/r/softwaregore/comments/uj0mdr/
+videoS = ".\page5.html" # https://www.reddit.com/r/PeopleFuckingDying/comments/uivfwf/
+videoNS = ".\page6.html"# https://www.reddit.com/r/GamePhysics/comments/uj5y0k/
+text = ".\page7.html"   # https://www.reddit.com/r/HomeNetworking/comments/tgboo8/
+error = ".\page4.html"  # https://redditsave.com/info?url=
 all = [
-    "C:\Projects\DisBot\page.html",
-    "C:\Projects\DisBot\page2.html",
-    "C:\Projects\DisBot\page3.html",
+    video,
+    gif,
+    image,
+    videoS,
+    videoNS,
+    text,
+    error,
 ]
-with open(video) as f:
-    soup = BeautifulSoup(f,"html.parser")
-    download = soup.find("div", {"class": "download-info"}).find("a") if not soup.find("div", {"class": "alert alert-danger"}) else None
+for i in all:
+    print()
+    with open(i) as f:
+        soup = BeautifulSoup(f,"html.parser")
+        download = soup.find("div", {"class": "download-info"}).find("a") if not soup.find("div", {"class": "alert alert-danger"}) else None
 
 
-videodet = [
-    ("download_sound_video","video"),
-    ("download_ireddit_gif","gif"),
-    ("download_generic","generic")
-    ]
+    videodet = [
+        ("download_sound_video","video-sound"),
+        ("download_ireddit_gif","gif"),
+        ("download_vreddit_no_sound","video-no-sound"),
+        ("download_generic","generic"),
+        ]
+    if download:
+        next = download.attrs["onclick"]
+        nexthref = download.get("href")
+        for erm in videodet:
+            if erm[0] in next:
+                break
+        pass
+        match erm[1]:
+            case "video-sound":
+                print(nexthref,erm[1])
+            case "video-no-sound":
+                print(nexthref,erm[1])
+            case "gif":
+                print(nexthref,erm[1])
+            case "generic":
+                print(nexthref,erm[1])
+            case _:
+                print("error",nexthref,erm[1])
 
-next = download.attrs["onclick"]
-for erm in videodet:
-    if erm[0] in next:
-        break
-print(erm[1])
 # print(download)
 # res = req.get(download)
 # print(res.headers)
